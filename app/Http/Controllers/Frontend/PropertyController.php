b9e2730637605d63251a51e40965c8fee844151a
farchandio <farchandio@gmail.com>
2024-06-10 18:08:19 +0700

    Create PropertyController.php

---
+++ app/Http/Controllers/Frontend/PropertyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PropertyController extends Controller
+{
+    public function index()
+    {
+    return view('frontend.property.index');
+    }
+
+    public function show ()
+    {
+    return view('frontend.property.single');
+    }
+}
